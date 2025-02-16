a22062262982c313ada33b2f6e2f32c860b19028
Justwatchingtv <114772825+Justwatchingtv@users.noreply.github.com>
2025-02-16 01:05:51 +0000

    Assistant checkpoint: Removed duplicate DialogFooter component
    Assistant generated file changes:
    - client/src/components/ui/dialog.tsx: Remove duplicate DialogFooter declaration
    
    ---
    
    User prompt:
    
    please fix this error

--- client/src/components/ui/dialog.tsx
+++ client/src/components/ui/dialog.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import * as React from "react"
@@ -109,19 +108,6 @@ const DialogDescription = React.forwardRef<
 ))
 DialogDescription.displayName = DialogPrimitive.Description.displayName
 
-const DialogFooter = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
-      className
-    )}
-    {...props}
-  />
-)
-DialogFooter.displayName = "DialogFooter"
 
 export {
   Dialog,
